f398bcbe56411d6c1aab9d19b90f0223d786f5bb
cosminbbx <virgacosmin@gmail.com>
2021-04-03 13:34:52 +0300

    EventPlanning step1

---
+++ DigitalEventPlaner.Web/Controllers/EventPlanningController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using DataLayer.Enumerations;
+using DigitalEventPlaner.Web.Models.EventPlanning;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalEventPlaner.Web.Controllers
+{
+    public class EventPlanningController : Controller
+    {
+        public EventPlanningController()
+        {
+        }
+
+        [Authorize(Roles = "Customer")]
+        public IActionResult Step1()
+        {
+            return View(new EventPlanningStep1ViewModel());
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Customer")]
+        public IActionResult Step1(EventPlanningStep1ViewModel model)
+        {
+            //var model = new EventPlanningStep1ViewModel();
+            //model.EventTypes = Enum.GetValues(typeof(ServiceType)).Cast<string>().ToList();
+            return View(model);
+        }
+    }
+}
---
+++ DigitalEventPlaner.Web/Models/EventPlanning/EventPlanningStep1ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using DataLayer.Enumerations;
+
+namespace DigitalEventPlaner.Web.Models.EventPlanning
+{
+    public class EventPlanningStep1ViewModel
+    {
+        public int UserId { get; set; }
+        [Required]
+        public string EventDate { get; set; }
+        [Required]
+        public List<string> EventTypes { get; set; }
+
+        [Required]
+        public List<string> EventTypesSelected { get; set; }
+
+        public string StringDateNow { get; set; }
+
+        public EventPlanningStep1ViewModel()
+        {
+            EventTypes = Enum.GetNames(typeof(ServiceType)).ToList();
+            StringDateNow = DateTime.Now.ToString("yyyy-MM-dd");
+        }
+    }
+}
+    
